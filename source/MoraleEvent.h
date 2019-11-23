ad2c0a6e110b86eef9668289f6a922d7f1d19043
Luke Arndt <lukearndt@users.noreply.github.com>
2019-11-23 18:38:29 +1300

    Add second draft of data file + MoraleEvent.h

---
+++ source/MoraleEvent.h
@@ -0,0 +1,45 @@
+/* MoraleEvent.h
+Copyright (c) 2019 by Luke Arndt
+
+Endless Sky is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later version.
+
+Endless Sky is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+*/
+
+#ifndef MORALE_EVENT_H_
+#define MORALE_EVENT_H_
+
+#include "Ship.h"
+
+class MoraleEvent
+{
+public:
+	// Load a definition for a morale event
+	void Load(const DataNode &node);
+	
+	double ChancePerMorale() const;
+	double Effect() const;
+	double Threshold() const;
+	const std::string &Id() const;
+	const std::string &Message() const;
+
+private:
+	// The base chance of an active event happening
+	double baseChance = 0;
+	// The chance of an active event increases per point of morale past the threshold
+	double chancePerMorale = 0;
+	// The event changes the ship's morale by this much when it occurs
+	double effect = 0;
+	// The morale at which an active event becomes possible
+	double threshold = 0;
+	// The id that the morale event is stored against in GameData::Crews()
+	std::string id;
+	// A message to display when the morale event occurs
+	std::string message;
+};
+
+#endif
